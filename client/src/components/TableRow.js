8a68a3fa4bcf195da477ef4c583e72f666bc7485
ntesalunaa <s3943408@student.rmit.edu.au>
2024-02-19 13:40:57 +1100

    created a 'TableRow' component for displaying product data

---
+++ client/src/components/TableRow.js
@@ -0,0 +1,38 @@
+import React from 'react'
+
+function TableRow({props}) {
+  const { product_title, product_price, product_save_price, product_special_offer_text, url } = props
+  console.log(product_title)
+  return (
+    // <tr className="border-b border-emerald-200 bg-emerald-100 text-neutral-800">
+    <>
+      <tr className={
+        (product_save_price === 0 && product_special_offer_text === '' ) ?
+        "border-b dark:border-neutral-500" :
+          (product_special_offer_text === '') ?
+          "border-b border-emerald-200 bg-emerald-100 text-neutral-800" : 
+          "border-b border-yellow-200 bg-yellow-100 text-neutral-800"
+      }>
+        <td className="px-6 py-4 font-medium">{product_title}</td>
+        <td className="px-6 py-4">${product_price}</td>
+        <td className="px-6 py-4">${(parseFloat(product_price) + parseFloat(props.product_save_price)).toFixed(2)}</td>
+        <td className="px-6 py-4">
+        {(product_save_price === 0 && product_special_offer_text === '' ) ? (
+          <>No special offer</>
+        ) : (
+          (product_special_offer_text === '') ? (
+            <>Save ${product_save_price} (<span className='text-emerald-500	'>-{((product_save_price / product_price) * 100).toFixed(2)}%</span>)</>
+          ) : (
+            <>{product_special_offer_text}</>
+          )
+        )}
+        </td>
+        <td className="px-6 py-4">
+          <a className='text-blue-500 hover:underline' href={url} target="_blank">Link</a>
+        </td>
+      </tr>
+    </>
+  )
+}
+
+export default TableRow
