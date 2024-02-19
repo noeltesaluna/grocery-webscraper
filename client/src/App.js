1bfbfbb9c7b4e5f9df89783bfecc123a661382f7
ntesalunaa <s3943408@student.rmit.edu.au>
2024-02-19 13:41:16 +1100

    added 'TableRow' component to display data

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,5 @@
 import { useState, useEffect } from "react";
+import TableRow from "./components/TableRow";
 
 export default function App() {
   const [inputValue, setInputValue] = useState('')
@@ -41,7 +42,7 @@ export default function App() {
     <div className="m-8 text-center"> 
       <h1 className="text-3xl font-bold">Shop Harvest 🛒</h1>
       <form className="flex flex-col	items-center" onSubmit={handleSubmit}>
-        <label className="m-2">Enter Product URL </label>
+        <label className="m-2 text-sm">Enter Product URL </label>
         <input 
           className="w-full py-2 px-4 border rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500" 
           type="text" 
@@ -50,12 +51,32 @@ export default function App() {
           onChange={handleChange}
         />
         <input 
-          className="mt-4 bg-green-500 hover:bg-green-600 text-white font-bold py-2 px-4 rounded focus:outline-none focus:ring-2 focus:ring-green-500" 
+          className="mt-4 text-sm bg-green-500 hover:bg-green-600 text-white font-bold py-2 px-4 rounded focus:outline-none focus:ring-2 focus:ring-green-500" 
           type="submit" 
           value="Add Product" 
         />
       </form>
-      <pre className="text-m">{JSON.stringify(productData, null, 2)}</pre>
+      { !productData.products_details ? (
+        <p>loading</p>
+      ) : (
+        <table className="min-w-full text-left text-xs font-light">
+          <thead className="border-b font-medium dark:border-neutral-500">
+            <tr>
+              <th className="px-6 py-4">Product</th>
+              <th className="px-6 py-4">Current Price</th>
+              <th className="px-6 py-4">Orignal Price</th>
+              <th className="px-6 py-4">Special Offer</th>
+              <th className="px-6 py-4">Link To Product</th>
+            </tr>
+          </thead>
+          <tbody>
+          {productData.products_details.map((product) =>
+            <TableRow props={product}/>
+          )}
+          </tbody>
+        </table>
+      )
+}
     </div>
   );
 }
