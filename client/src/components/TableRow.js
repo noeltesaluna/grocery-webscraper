818a9c2c45d7cfb508aaecf3f079f23471b9c14b
ntesalunaa <s3943408@student.rmit.edu.au>
2024-02-21 02:00:10 +1100

    fixed special offer percentage

--- client/src/components/TableRow.js
+++ client/src/components/TableRow.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 function TableRow({props}) {
   const { product_title, product_price, product_save_price, product_special_offer_text, url } = props
-  console.log(product_title)
+  let original_price = parseFloat(product_price) + parseFloat(product_save_price)
   return (
     // <tr className="border-b border-emerald-200 bg-emerald-100 text-neutral-800">
     <>
@@ -15,13 +15,13 @@ function TableRow({props}) {
       }>
         <td className="px-6 py-4 font-medium">{product_title}</td>
         <td className="px-6 py-4">${product_price}</td>
-        <td className="px-6 py-4">${(parseFloat(product_price) + parseFloat(props.product_save_price)).toFixed(2)}</td>
+        <td className="px-6 py-4">${original_price.toFixed(2)}</td>
         <td className="px-6 py-4">
         {(product_save_price === 0 && product_special_offer_text === '' ) ? (
           <>No special offer</>
         ) : (
           (product_special_offer_text === '') ? (
-            <>Save ${product_save_price} (<span className='text-emerald-500	'>-{((product_save_price / product_price) * 100).toFixed(2)}%</span>)</>
+            <>Save ${product_save_price} (<span className='text-emerald-500	'>-{(( 1 - (product_save_price / original_price)) * 100).toFixed(2)}%</span>)</>
           ) : (
             <>{product_special_offer_text}</>
           )
