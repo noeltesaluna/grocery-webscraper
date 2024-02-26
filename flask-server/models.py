40742b39f4f38f513921aaab4ccaaad9063a8388
noeltesaluna <ntesaluna03@gmail.com>
2024-02-26 18:56:46 +1100

    added 'model.py' and created a ProductListing model

---
+++ flask-server/models.py
@@ -0,0 +1,22 @@
+from config import db
+
+class ProductListing(db.Model):
+    id = db.Column(db.Integer, primary_key=True) 
+    code = db.Column(db.Integer, unique=False, nullable=False)
+    name = db.Column(db.String(1000), unique=False, nullable=False)
+    supermarket = db.Column(db.String(1000), unique=False, nullable=False)
+    price = db.Column(db.Float, unique=False, nullable=False)
+    price_off = db.Column(db.Float, unique=False)
+    offer_text = db.Column(db.String(1000), unique=False)
+
+    def to_json(self):
+        return {
+            "id": self.id,
+            "code": self.code,
+            "name": self.name,
+            "supermarket": self.supermarket,
+            "price": self.price,
+            "price_off": self.price_off,
+            "offer_text": self.offer_text
+        }
+    
