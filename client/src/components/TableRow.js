7f30fecb2ce1838bc1d0a233d1a02726bef122a9
noeltesaluna <ntesaluna03@gmail.com>
2024-02-26 20:10:50 +1100

    modified props for new backend implementation

--- client/src/components/TableRow.js
+++ client/src/components/TableRow.js
@@ -1,33 +1,33 @@
 import React from 'react'
 
 function TableRow({props}) {
-  const { product_title, product_price, product_save_price, product_special_offer_text, url } = props
-  let original_price = parseFloat(product_price) + parseFloat(product_save_price)
+  const { name, price, price_off, offer_text, code } = props
+  let original_price = parseFloat(price) + parseFloat(price_off)
   return (
     <>
       <tr className={
-        (product_save_price === 0 && product_special_offer_text === '' ) ?
+        (price_off === 0 && offer_text === '' ) ?
         "border-b dark:border-neutral-500" :
-          (product_special_offer_text === '') ?
+          (offer_text === '') ?
           "border-b border-emerald-200 bg-emerald-100 text-neutral-800" : 
           "border-b border-yellow-200 bg-yellow-100 text-neutral-800"
       }>
-        <td className="px-6 py-4 font-medium">{product_title}</td>
-        <td className="px-6 py-4">${product_price}</td>
+        <td className="px-6 py-4 font-medium">{name}</td>
+        <td className="px-6 py-4">${price.toFixed(2)}</td>
         <td className="px-6 py-4">${original_price.toFixed(2)}</td>
         <td className="px-6 py-4">
-        {(product_save_price === 0 && product_special_offer_text === '' ) ? (
+        {(price_off === 0 && offer_text === '' ) ? (
           <>No special offer</>
         ) : (
-          (product_special_offer_text === '') ? (
-            <>Save ${product_save_price} (<span className='text-emerald-500	'>-{(( 1 - (product_save_price / original_price)) * 100).toFixed(2)}%</span>)</>
+          (offer_text === '') ? (
+            <>Save ${price_off} (<span className='text-emerald-500	'>-{(( 1 - (price_off / original_price)) * 100).toFixed(2)}%</span>)</>
           ) : (
-            <>{product_special_offer_text}</>
+            <>{offer_text}</>
           )
         )}
         </td>
         <td className="px-6 py-4">
-          <a className='text-blue-500 hover:underline' href={url} target="_blank">Link</a>
+          <a className='text-blue-500 hover:underline' href={code} target="_blank" rel="noreferrer">Link</a>
         </td>
       </tr>
     </>
