adb97c8d31d8d88dbd1ffb84829ba9395052a19c
noeltesaluna <ntesaluna03@gmail.com>
2024-02-29 18:52:03 +1100

    edited the code field to be unique

--- flask-server/models.py
+++ flask-server/models.py
@@ -2,7 +2,7 @@ from config import db
 
 class ProductListing(db.Model):
     id = db.Column(db.Integer, primary_key=True) 
-    code = db.Column(db.Integer, unique=False, nullable=False)
+    code = db.Column(db.Integer, unique=True, nullable=False)
     name = db.Column(db.String(1000), unique=False, nullable=False)
     supermarket = db.Column(db.String(1000), unique=False, nullable=False)
     price = db.Column(db.Float, unique=False, nullable=False)
