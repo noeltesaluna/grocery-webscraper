eaac8508a65f39650101a760dfc90a6b340c2cb7
noeltesaluna <ntesaluna03@gmail.com>
2024-02-29 19:37:24 +1100

    removed and modularised creating coles product listings

--- flask-server/main.py
+++ flask-server/main.py
@@ -1,5 +1,6 @@
 from flask import request, jsonify
 from config import app, db
+from webscraper import coles_webscraper
 from models import ProductListing
 from bs4 import BeautifulSoup
 import requests
@@ -16,50 +17,9 @@ def get_productlistings():
 @app.route("/create_productlisting", methods=["POST"])
 def create_productlisting():
     URL = request.json.get("url")
-    page = requests.get(URL)
 
-    soup = BeautifulSoup(page.text, 'html.parser')
-
-    product_title = ""
-    code = ""
-
-    if soup.find(class_ = "product__title") is not None:
-        product_title = soup.find(class_ = "product__title").text
-        code = soup.find('p', attrs={'data-testid': 'product-code'}).text
-        code = int(code[code.find(": ") + 1:])
-    else: 
-        return (
-            jsonify({"message": "That product listing does not exist"}), 
-            400,
-        )
-
-    product_price = soup.find(class_ = "price__value").text
-    product_price = product_price[product_price.find("$") + 1:]
-    product_save_price = 0
-    product_special_offer_text = ""
-
-    # check if product is on special
-    if soup.find(class_ = "is-special") is not None:
-        
-        # check if product has special offer (i.e 2 for $10)
-        if soup.find('button', attrs={'data-testid': 'complex-promotion-link'}) is not None:
-            product_special_offer_text = soup.find('button', attrs={'data-testid': 'complex-promotion-link'}).text
-        
-        # check if proudct price drop (i.e "Save $0.25")
-        else:
-            product_save_price = soup.find('section', class_ = "badge-label").text
-            product_save_price = product_save_price[product_save_price.find("$") + 1:]
-
-    # TODO: Don't hardcode supermarket
-    new_productlisting = ProductListing(
-        code=code,
-        name=product_title,
-        supermarket="Coles",
-        price=product_price,
-        price_off=product_save_price,
-        offer_text=product_special_offer_text
-    )
     try:
+        new_productlisting = coles_webscraper(URL)
         db.session.add(new_productlisting)
         db.session.commit()
     except Exception as e:
