dcc1e31e041873b3c4159b6bc0ff6f1d75b2347c
noeltesaluna <ntesaluna03@gmail.com>
2024-03-09 15:22:25 +1100

    refactored main.py

--- flask-server/main.py
+++ flask-server/main.py
@@ -2,35 +2,30 @@ from flask import request, jsonify
 from config import app, db
 from webscraper import coles_webscraper
 from models import ProductListing
-from bs4 import BeautifulSoup
-import requests
-# create ProductListing
 
-@app.route("/productlistings", methods=["GET"])
-def get_productlistings():
-    # obtain product listings in the form of python objects
-    productlistings = ProductListing.query.all()
-    # convert product listings into python dictionary list
-    json_productlistings = list(map(lambda x: x.to_json(), productlistings))
-    return jsonify({"productlistings": json_productlistings})
+# GET all product listings
+@app.route("/product_listings", methods=["GET"])
+def get_product_listings():
+    product_listings = ProductListing.query.all()
+    json_product_listings = list(map(lambda x: x.to_json(), product_listings))
+    return jsonify({"product_listings": json_product_listings})
 
-@app.route("/create_productlisting", methods=["POST"])
-def create_productlisting():
+# Create a product listing
+@app.route("/product_listings", methods=["POST"])
+def create_product_listing():
     URL = request.json.get("url")
-
     try:
-        new_productlisting = coles_webscraper(URL)
-        db.session.add(new_productlisting)
+        new_product_listing = coles_webscraper(URL)
+        db.session.add(new_product_listing)
         db.session.commit()
     except Exception as e:
         return jsonify({"message": str(e)}), 400
-    
     return jsonify({"message": "Product listing created!"}), 201
 
+# Update all product listings
 @app.route("/update_productlistings", methods=["POST"])
 def update_productlistings():
     productlistings = ProductListing.query.all()
-
     try:
         for productlisting in productlistings:
             updatedProductlisting = coles_webscraper(productlisting.url)
@@ -40,12 +35,8 @@ def update_productlistings():
         db.session.commit()
     except Exception as e:
         return jsonify({"message": str(e)}), 400
-
     return jsonify({"message": "Updated product listings!"}), 201
-
         
-
-
 if __name__ == "__main__":
     with app.app_context():
         db.create_all()
